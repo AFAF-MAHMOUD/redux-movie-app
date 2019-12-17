df6375ff3fd55d8125c435f915e059c7afd3aeb1
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-17 13:41:06 +0100

    Updates

---
+++ precache-manifest.00d46f631dcfdadae7a7ea5d809beabc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2339066dcf8d44c335f3af3e9c9b94d9",
+    "url": "/redux-movie-app/index.html"
+  },
+  {
+    "revision": "9fe78e255b54c6e08a9a",
+    "url": "/redux-movie-app/static/css/main.3ba39aa0.chunk.css"
+  },
+  {
+    "revision": "144404172448dd7a7f3e",
+    "url": "/redux-movie-app/static/js/2.0ca7bdcb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux-movie-app/static/js/2.0ca7bdcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fe78e255b54c6e08a9a",
+    "url": "/redux-movie-app/static/js/main.e65c543e.chunk.js"
+  },
+  {
+    "revision": "3fc21a7dfa7a0b81d485",
+    "url": "/redux-movie-app/static/js/runtime-main.d695db4e.js"
+  }
+]);
